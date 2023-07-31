0cd8bca19959934e6c7458542029a1e4a7a63073
JoseRamirez <joseramirezgomez00@gmail.com>
2023-07-30 21:10:45 -0500

    add menu desplegable

--- desplegable/js/script.js
+++ desplegable/js/script.js
@@ -1,19 +1,29 @@
-
-const icon = document.getElementById(".nav-responsive");
+const icon = document.getElementById("icon");
+const select = document.querySelector(".seleccion");
 let menuVisible = false;
+
 function mostrarOcultarMenu() {
+    const navElement = document.getElementById("nav");
     if (menuVisible) {
-        document.getElementById("nav").classList = "";
+        navElement.classList.remove("responsive");
+        icon.classList.remove("fa-x");
+        icon.classList.add("fa-bars");
+        select.style.transform = "translateX(2px)";
         menuVisible = false;
     } else {
-        document.getElementById("nav").classList = "responsive"
+        navElement.classList.add("responsive");
+        icon.classList.remove("fa-bars");
+        icon.classList.add("fa-x");
+        select.style.transform = "translateX(130px)";
         menuVisible = true;
-        icon.classList.remove = 'fa-bars';
-        icon.classList.add = 'fa-x';
-        icon.classList.add = 'fa-solid';        
     }
 }
+
 function seleccionar() {
-    document.getElementById("nav").classList = "";
+    const navElement = document.getElementById("nav");
+    navElement.classList.remove("responsive");
+    icon.classList.remove("fa-x");
+    icon.classList.add("fa-bars");
+    select.style.transform = "translateX(2px)";
     menuVisible = false;
 }
